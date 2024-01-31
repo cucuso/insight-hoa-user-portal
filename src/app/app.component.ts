5f748543feed3c105a8baa651c463f3c08c2a087
cucuso <edenn001@gmail.com>
2024-01-31 08:14:38 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,39 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { FormBuilder } from '@angular/forms';
+import { User } from './user';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'insight-hoa-user-portal';
+
+  title = 'InsightHoa';
+  currentUser?: User;
+
+  checkoutForm = this.formBuilder.group({
+    firstName: '',
+    lastName: '',
+    email: ''
+  });
+
+  constructor(private httpClient: HttpClient,  private formBuilder: FormBuilder) { }
+
+  onSubmit() {
+    console.log('dd',this.checkoutForm.value);
+    this.httpClient.post<[any]>('http://localhost:8080/users', this.checkoutForm.value).subscribe(response => {
+      console.log('submitted user');
+    });
+
+    this.checkoutForm.reset();
+
+  }
 }
+
+
